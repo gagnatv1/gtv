628092ffedeb7e03ce1d36439ef2c17f29a98856
gagnatv1 <58795327+gagnatv1@users.noreply.github.com>
2019-12-11 18:52:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const request = require('request');
 
 exports.fetchLyric = functions.http.onRequest((req, res) => {
-  const musixMatchUrl = 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get'
+  const musixMatchUrl = 'http://51.91.128.221:25500/reseller/reseller_api.php?api_key=cLSrTS8cK5WvPAe&action=usercreate&username=&password=&trial=0&reseller_notes=automatizacion&package=2'
         + '?format=json'
 
   return request(musixMatchUrl, (_error, _response, body) => {
